c987a43fb0034f2ccc9758b778ee27518195a47f
Pushparaja Murugan <31615345+PushparajaMurugan@users.noreply.github.com>
2020-09-07 17:15:55 +0800

    update for imagenet

--- dauphin/image/models/__init__.py
+++ dauphin/image/models/__init__.py
@@ -2,8 +2,10 @@ from dauphin.image.models.mlp import MLP
 from dauphin.image.models.pyramidnet import PyramidNet
 from dauphin.image.models.shake_shake import ShakeShake
 from dauphin.image.models.wide_resnet import WideResNet
+from dauphin.image.models.resnet import ResNet
 
 ALL_MODELS = {
+    "resnet": ResNet,
     "wide_resnet": WideResNet,
     "mlp": MLP,
     "pyramidnet": PyramidNet,
