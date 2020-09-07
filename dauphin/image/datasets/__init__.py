6d1d565390fa81cc0a447ab99aaac417a06fff18
Pushparaja Murugan <31615345+PushparajaMurugan@users.noreply.github.com>
2020-09-07 17:08:58 +0800

    Update for imagenet dataloader

--- dauphin/image/datasets/__init__.py
+++ dauphin/image/datasets/__init__.py
@@ -1,7 +1,9 @@
 from dauphin.image.datasets.cifar_dataset import CIFARDataset
 from dauphin.image.datasets.mnist_dataset import MNISTDataset
+from dauphin.image.datasets.imagenet_dataset import ImageNetDataset
 
 ALL_DATASETS = {
+    "ImageNet": ImageNetDataset,
     "cifar10": CIFARDataset,
     "cifar100": CIFARDataset,
     "mnist": MNISTDataset,
